fbbc75abaa018212fffd904218588706f3d44332
Kobi Kadosh <kobi.kadosh@gmail.com>
2016-01-31 02:41:16 +0200

    chage controller to return json

--- app/controllers/teas_controller.rb
+++ app/controllers/teas_controller.rb
@@ -2,10 +2,12 @@ class TeasController < ApplicationController
 
   def index
     @teas = Tea.all
+    render json: @teas
   end
 
   def show
     @tea = Tea.find(params[:id])
+    render json: @tea
   end
 
   def new
